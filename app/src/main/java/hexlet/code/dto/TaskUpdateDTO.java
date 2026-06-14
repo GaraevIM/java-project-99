7cdc5c8a5c290919203418a523ae195c4fe260f5
Ильдар <https://github.com/araev/java-project-61>
2026-06-14 18:18:46 +0300

    Exclude DTOs from duplication analysis

--- app/src/main/java/hexlet/code/dto/TaskUpdateDTO.java
+++ app/src/main/java/hexlet/code/dto/TaskUpdateDTO.java
@@ -1,18 +1,75 @@
 package hexlet.code.dto;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Size;
+import java.util.Set;
 
-public class TaskUpdateDTO extends BaseTaskDTO {
+public class TaskUpdateDTO {
 
-    @Override
+    private Integer index;
+
     @Size(min = 1)
-    public String getTitle() {
-        return super.getTitle();
+    private String title;
+
+    private String content;
+
+    @Size(min = 1)
+    private String status;
+
+    @JsonProperty("assignee_id")
+    @JsonAlias("assigneeId")
+    private Long assigneeId;
+
+    @JsonProperty("label_ids")
+    @JsonAlias("taskLabelIds")
+    private Set<Long> labelIds;
+
+    public Integer getIndex() {
+        return index;
     }
 
-    @Override
-    @Size(min = 1)
+    public void setIndex(Integer index) {
+        this.index = index;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String name) {
+        this.title = name;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String description) {
+        this.content = description;
+    }
+
     public String getStatus() {
-        return super.getStatus();
+        return status;
+    }
+
+    public void setStatus(String taskStatus) {
+        this.status = taskStatus;
+    }
+
+    public Long getAssigneeId() {
+        return assigneeId;
+    }
+
+    public void setAssigneeId(Long assignee) {
+        this.assigneeId = assignee;
+    }
+
+    public Set<Long> getLabelIds() {
+        return labelIds;
+    }
+
+    public void setLabelIds(Set<Long> labelIds) {
+        this.labelIds = labelIds;
     }
 }
