178bdff17401082b2003de88397d73ff98790bcc
Ильдар <https://github.com/araev/java-project-61>
2026-07-10 23:54:49 +0300

    Fix N+1 query for tasks

--- app/src/main/java/hexlet/code/specification/TaskSpecification.java
+++ app/src/main/java/hexlet/code/specification/TaskSpecification.java
@@ -15,16 +15,32 @@ public final class TaskSpecification {
             String status,
             Long labelId
     ) {
-        return Specification.where(titleContains(titleCont))
+        return Specification.where(fetchRelations())
+                .and(titleContains(titleCont))
                 .and(hasAssignee(assigneeId))
                 .and(hasStatus(status))
                 .and(hasLabel(labelId));
     }
 
+    private static Specification<Task> fetchRelations() {
+        return (root, query, builder) -> {
+            var resultType = query.getResultType();
+
+            if (resultType != Long.class && resultType != long.class) {
+                root.fetch("taskStatus", JoinType.LEFT);
+                root.fetch("assignee", JoinType.LEFT);
+                root.fetch("labels", JoinType.LEFT);
+                query.distinct(true);
+            }
+
+            return builder.conjunction();
+        };
+    }
+
     private static Specification<Task> titleContains(String titleCont) {
         return (root, query, builder) -> {
             if (titleCont == null || titleCont.isBlank()) {
-                return null;
+                return builder.conjunction();
             }
 
             return builder.like(
@@ -37,7 +53,7 @@ public final class TaskSpecification {
     private static Specification<Task> hasAssignee(Long assigneeId) {
         return (root, query, builder) -> {
             if (assigneeId == null) {
-                return null;
+                return builder.conjunction();
             }
 
             return builder.equal(root.get("assignee").get("id"), assigneeId);
@@ -47,7 +63,7 @@ public final class TaskSpecification {
     private static Specification<Task> hasStatus(String status) {
         return (root, query, builder) -> {
             if (status == null || status.isBlank()) {
-                return null;
+                return builder.conjunction();
             }
 
             return builder.equal(root.get("taskStatus").get("slug"), status);
@@ -57,11 +73,12 @@ public final class TaskSpecification {
     private static Specification<Task> hasLabel(Long labelId) {
         return (root, query, builder) -> {
             if (labelId == null) {
-                return null;
+                return builder.conjunction();
             }
 
             query.distinct(true);
             var labels = root.join("labels", JoinType.INNER);
+
             return builder.equal(labels.get("id"), labelId);
         };
     }
